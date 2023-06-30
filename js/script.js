4e505afbcf6dd85228a8e980c32e5bb1315ff3a9
teachAlexIt <alekseiteacherit@gmail.com>
2023-06-30 13:19:06 +0300

    finish site

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-const button = document.querySelector('.animationButton');
+const flags = document.querySelectorAll('.fa-flag');
+const progresSpan = document.querySelector('#progress');
+const levelSpan = document.querySelector('#level');
+const animationButtons = document.querySelectorAll('.animationButton');
 
 function handleMouseMove(event) {
-  const rect = button.getBoundingClientRect();
+  const rect = event.target.getBoundingClientRect();
   const mouseX = event.clientX - rect.left;
   const mouseY = event.clientY - rect.top;
   const rotateX = (mouseY / rect.height - 0.5) * -40; // Adjust rotation sensitivity
   const rotateY = (mouseX / rect.width - 0.5) * 40; // Adjust rotation sensitivity
-  button.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  event.target.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 }
 
-button.addEventListener('mousemove', handleMouseMove);
-button.addEventListener('mouseleave', () => {
-  button.style.transform = 'perspective(500px) rotateX(0) rotateY(0)';
-});
+for (let b = 0; b < animationButtons.length; b++) {
+  animationButtons[b].addEventListener('mousemove', handleMouseMove);
+  animationButtons[b].addEventListener('mouseleave', (e) => {
+    e.target.style.transform = 'perspective(500px) rotateX(0) rotateY(0)';
+  });
+}
+
+const levelFinishButtons = document.querySelectorAll('.level-finish');
+for (let f = 0; f < levelFinishButtons.length; f++) {
+  levelFinishButtons[f].onclick = function () {
+    let levelNumber = f + 1;
+    let nextLevelClass = 'flag' + levelNumber;
+    for (let x = 0; x < flags.length; x++) {
+      flags[x].classList.remove('_marked');
+      if (flags[x].classList.contains(nextLevelClass)) {
+        flags[x].classList.add('_marked');
+        localStorage.setItem('flagsNumber', x);
+        break
+      } else {
+        localStorage.setItem('flagsNumber', 100);
+      }
+    }
+    localStorage.setItem('progressNumber', 100);
+    localStorage.setItem('levelNumber', levelNumber);
+
+    progresSpan.innerText = 100;
+    levelSpan.innerText = levelNumber;
+  }
+}
 
 
 
@@ -52,13 +80,19 @@ function chengeHeight(item) {
 
 
 // 
-const flags = document.querySelectorAll('.fa-flag');
-const progresSpan = document.querySelector('#progress');
+
 
 if (localStorage.getItem('flagsNumber')) {
-  flags[localStorage.getItem('flagsNumber')].classList.add('_marked');
-  progresSpan.innerText = Math.floor((100 / flags.length) * localStorage.getItem('flagsNumber'));
-}else{
+  if (localStorage.getItem('flagsNumber') != 100) {
+    flags[localStorage.getItem('flagsNumber')].classList.add('_marked');
+    progresSpan.innerText = localStorage.getItem('progressNumber');
+    levelSpan.innerText = localStorage.getItem('levelNumber');
+  } else {
+    for (let i = 0; i < flags.length; i++) {
+      flags[i].classList.remove('_marked');
+    }
+  }
+} else {
   flags[0].classList.add('_marked');
 }
 
@@ -68,8 +102,32 @@ for (let i = 0; i < flags.length; i++) {
       flags[i].classList.remove('_marked');
     }
     flags[i].classList.add('_marked');
-    progresSpan.innerText = Math.floor((100 / flags.length) * i);
-    localStorage.setItem('flagsNumber', i);
+
+    if (flags[i].classList.contains('flag0')) {
+      let levelFlags = document.querySelectorAll('.flag0');
+      let progressNumber = Math.floor((100 / (levelFlags.length)) * i);
+      progresSpan.innerText = progressNumber;
+      localStorage.setItem('flagsNumber', i);
+      localStorage.setItem('progressNumber', progressNumber);
+      localStorage.setItem('levelNumber', 1);
+
+    } else if (flags[i].classList.contains('flag1')) {
+      let levelFlags = document.querySelectorAll('.flag1');
+      for (let y = 0; y < levelFlags.length; y++) {
+        if (levelFlags[y].classList.contains('_marked')) {
+          let progressNumber = Math.floor((100 / (levelFlags.length)) * y);
+          progresSpan.innerText = progressNumber;
+          localStorage.setItem('flagsNumber', i);
+          localStorage.setItem('progressNumber', progressNumber);
+          localStorage.setItem('levelNumber', 2);
+        }
+      }
+    }
+
+    progresSpan.innerText = localStorage.getItem('progressNumber');
+    levelSpan.innerText = localStorage.getItem('levelNumber');
+
+
   }
 }
 
@@ -77,35 +135,33 @@ for (let i = 0; i < flags.length; i++) {
 
 
 // 
-// Select all the iframes on your page
 const iframes = document.querySelectorAll('iframe');
 const loadSpan = document.querySelector('.load-box span');
 let body = document.querySelector('body');
-// Counter to keep track of loaded iframes
 let loadedCount = 0;
 
-// Function to check if all iframes have loaded
 function checkAllIframesLoaded() {
   if (loadedCount === iframes.length) {
-    // All iframes have loaded, do something here
     setTimeout(() => {
       body.classList.remove('scroll_none');
     }, 1000);
   }
 }
 
-// Loop through each iframe and add an event listener for the 'load' event
+
+
 iframes.forEach(iframe => {
   iframe.addEventListener('load', () => {
-    // Increment the loaded count
     loadedCount++;
     loadMove()
-    // Check if all iframes have loaded
     checkAllIframesLoaded();
   });
 
-  function loadMove(){
+  function loadMove() {
     let procent = 100 / iframes.length * loadedCount;
     loadSpan.style.width = procent + '%';
   }
 });
+
+
+
--- js/store.js
+++ js/store.js
@@ -1,62 +1,70 @@
+window.scrollTo(0, 0);
+if (performance.navigation.type === 1) {
+  window.scrollTo(0, 0);
+  window.location.hash = 'start';
+  // history.replaceState({}, document.title, window.location.pathname + window.location.search);
+}
+
+
 const lessonsStore = [
   lessonsBlockOneStore = [
     {
-      lessonsName: '№1 Знакомство с HTML',
+      lessonsName: '№1.1 Знакомство с HTML',
       lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.1/f1.1.html'
     },
     {
-      lessonsName: '№2 Новые теги',
+      lessonsName: '№1.2 Новые теги',
       lessonsLinkVideo: 'https://www.youtube.com/embed/5NA06q-APCI',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.2/f1.2.html'
     },
     {
-      lessonsName: '№3 Знакомство с CSS',
+      lessonsName: '№1.3 Знакомство с CSS',
       lessonsLinkVideo: 'https://www.youtube.com/embed/345P4oJlnkU',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.3/f1.3.html'
     },
     {
-      lessonsName: '№4 Секции',
+      lessonsName: '№1.4 Секции',
       lessonsLinkVideo: 'https://www.youtube.com/embed/GTdZrzxMkKs',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.4/f1.4.html'
     },
     {
-      lessonsName: '№5 Фото-фон',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
+      lessonsName: '№1.5 Фото-фон',
+      lessonsLinkVideo: 'https://www.youtube.com/embed/e4CV9qFDFnM',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.5/f1.5.html'
     },
     {
-      lessonsName: '№6 Шрифты',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
+      lessonsName: '№1.6 Шрифты',
+      lessonsLinkVideo: 'https://www.youtube.com/embed/_EUchkdlC-M',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.6/f1.6.html'
     },
     {
-      lessonsName: '№7 GitHub',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
+      lessonsName: '№1.7 GitHub',
+      lessonsLinkVideo: 'https://teachalexit.github.io/foto/img/1.svg',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.7/f1.7.html'
     },
     {
-      lessonsName: '№8 Многостраничность',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
+      lessonsName: '№1.8 Многостраничность',
+      lessonsLinkVideo: 'https://teachalexit.github.io/foto/img/1.svg',
       lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.8/f1.8.html'
     },
   ],
 
   lessonsBlockTwoStore = [
     {
-      lessonsName: 'Урок №1',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
-      lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.1/f1.1.html'
+      lessonsName: 'Урок №2.1',
+      lessonsLinkVideo: 'https://teachalexit.github.io/foto/img/1.svg',
+      lessonsLinkSample: 'https://teachalexit.github.io/manuals/404.html'
     },
     {
-      lessonsName: 'Урок №2',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
-      lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.2/f1.2.html'
+      lessonsName: 'Урок №2.2',
+      lessonsLinkVideo: 'https://teachalexit.github.io/foto/img/1.svg',
+      lessonsLinkSample: 'https://teachalexit.github.io/manuals/404.html'
     },
     {
-      lessonsName: 'Урок №3',
-      lessonsLinkVideo: 'https://www.youtube.com/embed/2BBxEBNo45Q',
-      lessonsLinkSample: 'https://teachalexit.github.io/manuals/frontend/f1.2/f1.2.html'
+      lessonsName: 'Урок №2.3',
+      lessonsLinkVideo: 'https://teachalexit.github.io/foto/img/1.svg',
+      lessonsLinkSample: 'https://teachalexit.github.io/manuals/404.html'
     },
   ],
 ]
@@ -68,37 +76,39 @@ const lessonsListTwoDiv = document.querySelector('.twoBlok .lessons__list');
 render(lessonsListOneDiv, 0);
 render(lessonsListTwoDiv, 1);
 
-function render(lessonsList, numberStoreBlock){
+function render(lessonsList, numberStoreBlock) {
   console.log(lessonsStore[numberStoreBlock]);
   for (let i = 0; i < lessonsStore[numberStoreBlock].length; i++) {
     let lessonsItemDiv = document.createElement('div');
     lessonsItemDiv.setAttribute('class', 'lessons__item');
-  
+
     let lessonsNameDiv = document.createElement('div');
     lessonsNameDiv.setAttribute('class', 'lessons__name');
-  
+
     let icon = document.createElement('i');
-    icon.setAttribute('class', 'fa-solid fa-flag');
-  
+    let test = 'flag' + numberStoreBlock;
+    icon.setAttribute('class', 'fa-solid fa-flag ' + test);
+
     let lessonsNameH3 = document.createElement('h3');
     lessonsNameH3.innerText = lessonsStore[numberStoreBlock][i].lessonsName;
-  
+
     let lessonsVideoIframe = document.createElement('iframe');
     lessonsVideoIframe.setAttribute('allowfullscreen', 'true')
     lessonsVideoIframe.setAttribute('src', lessonsStore[numberStoreBlock][i].lessonsLinkVideo);
-  
+    lessonsVideoIframe.cookie = "cookieName=cookieValue; SameSite=Lax";
+
     let lessonsSampleA = document.createElement('a');
     lessonsSampleA.setAttribute('class', '_sample');
     lessonsSampleA.setAttribute('target', '_blank');
     lessonsSampleA.setAttribute('href', lessonsStore[numberStoreBlock][i].lessonsLinkSample);
     lessonsSampleA.innerText = 'Образец';
-  
+
     lessonsNameDiv.appendChild(icon);
     lessonsNameDiv.appendChild(lessonsNameH3);
     lessonsItemDiv.appendChild(lessonsNameDiv);
     lessonsItemDiv.appendChild(lessonsVideoIframe);
     lessonsItemDiv.appendChild(lessonsSampleA);
-  
+
     lessonsList.appendChild(lessonsItemDiv);
   }
 };
